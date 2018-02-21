e924c22531b57e15479e0140ed07f049010f9705
James York <james@grandcircus.co>
2018-02-21 15:42:51 -0500

    change tabs component to display weather info on load

--- src/tabs/ContentTabs.js
+++ src/tabs/ContentTabs.js
@@ -3,21 +3,53 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import './ContentTabs.css'
 import wunderground from '../services/wunderground';
+import location from '../services/location';
+import foreman from '../services/foreman';
 
-export default () => ( 
-  <Tabs className="Tabs">
-    <TabList>
-      <Tab>Current Temperature</Tab>
-      <Tab>10 Day Temperatures</Tab>
-    </TabList>
+class ContentTabs extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      current: {
+        temp: '-',
+        feelsLike: '-',
+        icon: ''
+      }
+    }
+    foreman.getCurrent()
+      .then((response) => {
+        this.setState({
+          current: {
+            temp: response.temp,
+            feelsLike: response.feelLike,
+            icon: response.icon
+          }
+        })
+      })
+    
+  }
 
-    <TabPanel>
-      <h2>Any content 1</h2>
-      <button onClick= { () => console.log(wunderground.getCurrentForecast()) } >Click for Stuff</button>
-    </TabPanel>
-    <TabPanel>
-      <h2>Any content 2</h2>
-      <button onClick= { () => console.log(wunderground.get10DayForecast()) } >Click for More Stuff</button>
-    </TabPanel>
-  </Tabs>
-);
+  render(){
+    return ( 
+      <Tabs className="Tabs"  >
+        <TabList>
+          <Tab>Current Temperature</Tab>
+          <Tab>10 Day Temperatures</Tab>
+        </TabList>
+          <h2>Current Conditions</h2>
+          <h1 className="temp">{ this.state.current.temp }</h1>
+          <img src={this.state.current.icon} />
+          <h3>Feels like: { this.state.current.feelsLike }</h3>
+        <TabPanel>
+          
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+          <button onClick= { () => console.log(wunderground.get10DayForecast()) } >Click for More Stuff</button>
+        </TabPanel>
+      </Tabs>
+    )
+  }
+}
+
+export default ContentTabs;
