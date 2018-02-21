f805900ff2789cc2f3e99b366a7135c669384b85
James York <james@grandcircus.co>
2018-02-21 15:42:05 -0500

    making calls to wunderground from service

--- src/services/wunderground.js
+++ src/services/wunderground.js
@@ -2,15 +2,18 @@ import axios from 'axios';
 
 let current = {};
 let tenDay  = {};
+const API_KEY = '845c01b7b50bde40'; // I know, I know. API key in the client is bad. Mea Culpa.
 
-async function getCurrentForecast(){
-  const result = await axios.get('http://reddit.com/.json')
-  return Object.assign(current, result.data.data);
+function getCurrentForecast(coords){
+  console.log(coords);
+  return axios.get(`http://api.wunderground.com/api/${API_KEY}/conditions/q/${coords.latitude},${coords.longitude}.json`);
+  
 }
 
 async function get10DayForecast(){
   const result = await axios.get('http://reddit.com/.json')
   return Object.assign(tenDay, result.data.data);
 }
+
 
 export default { getCurrentForecast, get10DayForecast }; 
