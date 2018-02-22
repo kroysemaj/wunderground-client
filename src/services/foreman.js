dde000a20491babd0ac79c6646e7828b966d0dde
James York <james@grandcircus.co>
2018-02-22 11:35:57 -0500

    wiring up 10 day call to wunderground

--- src/services/foreman.js
+++ src/services/foreman.js
@@ -17,7 +17,8 @@ function getCurrent(){
             const curr =  {
               temp: parseInt(weather.temp_f, 10),
               feelLike: weather.feelslike_f,
-              icon: weather.icon_url
+              icon: weather.icon_url,
+              iconText: weather.icon,
             };
             console.log('weather: ', curr)
             return curr;
@@ -26,7 +27,15 @@ function getCurrent(){
 }
 
 function get10Day(){
-
-  const tenDayWeather = wunderground.get10DayForecast(loc);
-  return tenDayWeather;
+  console.log('getting 10 day');
+  return  location()
+          .then( (position) => {
+            console.log('position: ', position);
+            return wunderground.get10DayForecast(position.coords);
+          })
+          .then( (response) => {
+            const forecasts = response.data.forecast.simpleforecast.forecastday;
+            return forecasts;
+          })
+          .catch( (err) => console.error(err) );
 }
--- src/services/wunderground.js
+++ src/services/wunderground.js
@@ -1,18 +1,16 @@
 import axios from 'axios';
 
-let current = {};
 let tenDay  = {};
 const API_KEY = '845c01b7b50bde40'; // I know, I know. API key in the client is bad. Mea Culpa.
 
 function getCurrentForecast(coords){
   console.log(coords);
   return axios.get(`http://api.wunderground.com/api/${API_KEY}/conditions/q/${coords.latitude},${coords.longitude}.json`);
-  
 }
 
-async function get10DayForecast(){
-  const result = await axios.get('http://reddit.com/.json')
-  return Object.assign(tenDay, result.data.data);
+function get10DayForecast(coords){
+  console.log(coords);
+  return axios.get(`http://api.wunderground.com/api/${API_KEY}/forecast10day/q/${coords.latitude},${coords.longitude}.json`);
 }
 
 
