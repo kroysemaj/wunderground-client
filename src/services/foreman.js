e071f17a8e65f477729562d6e1f86662442d9a4d
James York <james@grandcircus.co>
2018-02-23 16:28:02 -0500

    Add geocode lookup to wunderground api

--- src/services/foreman.js
+++ src/services/foreman.js
@@ -1,52 +1,79 @@
 import location from './location';
 import wunderground from './wunderground';
 
-export default { getCurrent, get10Day };
+export default { getCurrent, get10Day, geoLookup };
 
 function getCurrent(){
   console.log('getting current');
   return  location()
-          .then( 
-            (position) => {
-              return wunderground.getCurrentForecast(position.coords);
-            }, 
-            () => wunderground.getCurrentForecast() ) //On a locations rejection, call using default coords
-          .then ((response) => {
-            const weather = response.data.current_observation;
-            const curr =  {
-              temp: parseInt(weather.temp_f, 10),
-              feelLike: weather.feelslike_f,
-              icon: weather.icon_url,
-              iconText: weather.icon,
-            };
-            console.log('weather: ', curr)
-            return curr;
-          })
-          .catch( (err)=> console.error(err.message) );
+            .then( 
+              (position) => {
+                return wunderground.getCurrentForecast(position.coords);
+              }, 
+              () => wunderground.getCurrentForecast() ) //On a locations rejection, call using default coords
+            .then ((response) => {
+              const weather = response.data.current_observation;
+              const curr =  {
+                temp: parseInt(weather.temp_f, 10),
+                feelLike: weather.feelslike_f,
+                icon: weather.icon_url,
+                iconText: weather.icon,
+              };
+              return curr;
+            })
+            .catch( (err)=> console.error(err.message) );
 }
 
 function get10Day(){
   console.log('getting 10 day');
   return  location()
-          .then( 
-            (position) => {
-              return wunderground.get10DayForecast(position.coords);
-            }, 
-            () => wunderground.get10DayForecast() ) //On a locations rejection, call using default coords
-          .then( (response) => {
-            const forecasts = response.data.forecast.simpleforecast.forecastday;
-            const tenDay = forecasts.map( ( forecast ) => {
-              return {
-                period: forecast.period,
-                epoch: forecast.date.epoch,
-                date: forecast.date.weekday_short,
-                low: forecast.low.fahrenheit,
-                high: forecast.high.fahrenheit,
-                icon: forecast.icon_url,
-                iconText: forecast.icon
-              };
-            });
-            return tenDay;
-          })
-          .catch( (err) => console.error(err) );
+            .then( 
+              (position) => {
+                return wunderground.get10DayForecast(position.coords);
+              }, 
+              () => wunderground.get10DayForecast() ) //On a locations rejection, call using default coords
+            .then( (response) => {
+              const forecasts = response.data.forecast.simpleforecast.forecastday;
+              const tenDay = forecasts.map( ( forecast ) => {
+                return {
+                  period: forecast.period,
+                  epoch: forecast.date.epoch,
+                  date: forecast.date.weekday_short,
+                  low: forecast.low.fahrenheit,
+                  high: forecast.high.fahrenheit,
+                  icon: forecast.icon_url,
+                  iconText: forecast.icon
+                };
+              });
+              return tenDay;
+            })
+            .catch( (err) => console.error(err) );
+}
+
+function geoLookup(){
+  console.log('getting location');
+  return  location()
+            .then( 
+              (position) => {
+                return wunderground.getLocationFromCoords(position.coords);
+              }, 
+              () => wunderground.getLocationFromCoords() ) //On a locations rejection, call using default coords
+            .then( (response) => {
+              const locationData = response.data.location;
+              return prepareLookupData(locationData);
+            })
+            .catch( (err) => console.error(err) );
+}
+
+function prepareLookupData(locationData) {
+  console.log("Location Data: ", locationData);
+  let location = {
+    city: locationData.city
+  }
+  if(locationData.country_name !== 'USA'){
+    location.state = locationData.country_name; // for international cities, "state" becomes country. Ex Paris, France
+  } else {
+    location.state = locationData.state;
+  }
+  return location; 
 }
--- src/services/wunderground.js
+++ src/services/wunderground.js
@@ -2,14 +2,15 @@ import axios from 'axios';
 
 const API_KEY = '845c01b7b50bde40'; // I know, I know. API key in the client is bad. Mea Culpa.
 
-const defaultCoords = {
-  latitude: -33.9249,
-  longitude: 18.4241
+const defaultCoords = { // Honolulu, HI
+  // latitude: 21.3069,
+  // longitude: -157.8583
+  latitude: 51.4545,  // international test data. Bristol, UK
+  longitude: -2.5879
 };
 
 function getCurrentForecast(coords){
   if (!coords){
-    console.log("no coords, using defaults");
     coords = defaultCoords;
   }
   return axios.get(`http://api.wunderground.com/api/${API_KEY}/conditions/q/${coords.latitude},${coords.longitude}.json`);
@@ -17,11 +18,16 @@ function getCurrentForecast(coords){
 
 function get10DayForecast(coords){
   if (!coords){
-    console.log("no coords, using defaults");
     coords = defaultCoords;
   }
   return axios.get(`http://api.wunderground.com/api/${API_KEY}/forecast10day/q/${coords.latitude},${coords.longitude}.json`);
 }
 
+function getLocationFromCoords(coords){
+  if(!coords){
+    coords = defaultCoords;
+  }
+  return axios.get(`http://api.wunderground.com/api/${API_KEY}/geolookup/q/${coords.latitude},${coords.longitude}.json`);
+}
 
-export default { getCurrentForecast, get10DayForecast }; 
+export default { getCurrentForecast, get10DayForecast, getLocationFromCoords }; 
